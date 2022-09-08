d8e2448193b4138ba0de33bc962dfaf0c843795c
Divakar <divakar39312@gmail.com>
2022-09-08 09:33:25 +0530

    [Divakar]:return sad

--- MoodAnalyzer/src/main/java/com/Bridgelabz/MoodAnalyzer.java
+++ MoodAnalyzer/src/main/java/com/Bridgelabz/MoodAnalyzer.java
@@ -1,19 +1,33 @@
 package com.Bridgelabz;
 
-public class MoodAnalyzer {
+//public class MoodAnalyzer {
     //Welcome Function
-    public static void welcome() {
-        System.out.println("Welcome to Mood Analyzer");
-    }
+//    public static void welcome(){
+//        System.out.println("Welcome to Mood Analyzer");
+//
+//    }
+    //Function to Check Mood of a String
+//    public String moodCheck(String message){
+//        if(message.contains("Sad")) {
+//            System.out.println("In SAD Mood");
+//            return "SAD";
+//        }
+//        else{
+//            System.out.println("In HAPPY Mood");
+//            return "HAPPY";
+//        }
+//    }
+//}
 
-    //Function to Check Mood of a String Entered
-    public String moodCheck(String message) {
-        if (message.contains("SAD")) {
-            System.out.println("In SAD Mood");
-            return "SAD";
-        } else {
-            System.out.println("In HAPPY Mood");
+
+public class MoodAnalyzer {
+    public String analyseMood(String message) {
+        if (message.contains("Any Mood")){
             return "HAPPY";
+        }
+        else {
+            return "SAD";
         }
     }
 }
+
--- MoodAnalyzer/src/test/java/com/Bridgelabz/MoodAnalyzerTest.java
+++ MoodAnalyzer/src/test/java/com/Bridgelabz/MoodAnalyzerTest.java
@@ -2,16 +2,26 @@ package com.Bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+//public class MoodAnalyzerTest {
+//    MoodAnalyzer moodAnalyze = new MoodAnalyzer();
+//
+//    @Test
+//    public void if_MessageWasSad_ShouldReturnSad(){
+//        moodAnalyze.welcome();
+//        String message = "I am in Happy Mood";
+//        String result = moodAnalyze.moodCheck(message);
+//        Assertions.assertEquals("SAD",result);
+//    }
+//}
+
+
 public class MoodAnalyzerTest {
-    MoodAnalyzer moodAnalyze = new MoodAnalyzer();
-
     @Test
-    public void if_MessageWasSad_ShouldReturnSad() {
-        moodAnalyze.welcome();
-
-        String message = "I am in SAD Mood";
-        String mood = moodAnalyze.moodCheck(message);
-        Assertions.assertEquals("SAD", mood);
-
+    public void givenMessage_IsProper_ShouldReturnSad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String actualResult = moodAnalyzer.analyseMood("I am in Sad Mood");
+        Assertions.assertEquals("SAD", actualResult);
     }
+
+
 }
