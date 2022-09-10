722562f0d076a47b2e727d3a1102f3d318b4dda2
Divakar <divakar39312@gmail.com>
2022-09-10 21:22:11 +0530

    [Divakar]:should return happy

--- MoodAnalyzer/src/test/java/com/Bridgelabz/MoodAnalyzerTest.java
+++ MoodAnalyzer/src/test/java/com/Bridgelabz/MoodAnalyzerTest.java
@@ -20,20 +20,20 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyzerTest {
 
-    @Test
-        // Test case for return SAD if message contains sad
-    void messageSad_returnSad(){
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in sad mood");
-        String result = moodAnalyzer.analyzeMood();
-        System.out.println(result);
-        Assertions.assertEquals("SAD" , result);
-    }
-//    @Test
-//    // Test case for Return Happy if message doesnot contain sad
-//    void messageAnyThing_ReturnHappy(){
-//        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any mood");
+    //    @Test
+//    // Test case for return SAD if message contains sad
+//    void messageSad_returnSad(){
+//        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in sad mood");
 //        String result = moodAnalyzer.analyzeMood();
 //        System.out.println(result);
-//        Assertions.assertEquals("HAPPY", result);
-//   }
+//        Assertions.assertEquals("SAD" , result);
+//    }
+    @Test
+    // Test case for Return Happy if message contains happy
+    void messageAnyThing_ReturnHappy(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in happy mood");
+        String result = moodAnalyzer.analyzeMood();
+        System.out.println(result);
+        Assertions.assertEquals("HAPPY", result);
+    }
 }
